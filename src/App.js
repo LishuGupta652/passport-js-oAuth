087480b54bc0cc0c8f5679d4009c9b57dfe4638b
lishu <lishugupta652@gmail.com>
2021-11-30 00:10:21 +0530

    ROUTER INSTALLED

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Post from "./pages/Post";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
