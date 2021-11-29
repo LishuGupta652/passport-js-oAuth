9ef0db53673728802b3ec5a8d3131dd59cae1aa5
lishu <lishugupta652@gmail.com>
2021-11-29 18:27:09 +0530

    terminal

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="logo">AdminAuth</span>
+            <ul className="list">
+                <li className="listItem">
+                    <img src="" alt="" className="avatar" />
+                </li>
+                <li className="listItem">JohnDoe</li>
+                <li className="listItem">Logout</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
