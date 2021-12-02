fa4c59152e748f40b3272361cf51abc416c4e4f6
lishu <lishugupta652@gmail.com>
2021-12-02 21:18:38 +0530

    terminal

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["lishu"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost/3000",
+    method: "GET,POST,PPUT,DELETE",
+    Credential: true,
+  })
+);
 
 app.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
